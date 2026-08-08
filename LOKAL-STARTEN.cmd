bb38e54f2f8ad8cf79e245410bb57c01f0509ffc
Sch1nkenwurst <moritzlucht7@gmail.com>
2026-08-08 15:17:13 +0200

    Add LOKAL-STARTEN.cmd for cross-device development

---
+++ LOKAL-STARTEN.cmd
@@ -0,0 +1,6 @@
+@echo off
+title Card Collector - lokale Testversion
+cd /d "%~dp0"
+start "" powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 1; Start-Process 'http://localhost:4173'"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0start.ps1"
+
